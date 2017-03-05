fd59a42bf6912196638f0991f9c46813b4a9aca4
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2017-03-05 20:20:17 +0100

    More verbose harmony connection script.

--- usr/local/bin/harmony.sh
+++ usr/local/bin/harmony.sh
@@ -5,8 +5,11 @@ address="00:04:20:F5:FA:B7"
 while (sleep 1)
 do
    connected=`hidd --show` > /dev/null
-   if [[ ! $connected =~ .*${address}.* ]]
+   if [[ ! $connected =~ .*$address.* ]]
    then
-      hidd --connect ${address} > /dev/null 2>&1
+      echo "Trying to connect to $address ..."
+      hidd --connect $address > /dev/null 2>&1
+   else
+      echo "Connected to $address."
    fi
 done
