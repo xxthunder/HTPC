b5c5091bfbdcb8f555c1091baa49d182f967217d
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2018-01-04 22:20:35 +0100

    VDR and DVB config via Ansible

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 SETUPROOT=$(pushd $(dirname $0) > /dev/null; pwd -P)
 
 pushd $SETUPROOT
+
+# Run ansible playbooks
 ansible-playbook test_sudo.yml vdr.yml kodi.yml
+
+# Run old setup routines
+sudo ./bin/setup.sh
+
 popd
 
