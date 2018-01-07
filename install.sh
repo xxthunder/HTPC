aa3e775c94237dceeed0d6953162650384abc14f
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2018-01-07 23:35:14 +0100

    Some system config changes via Ansible

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SETUPROOT=$(pushd $(dirname $0) > /dev/null; pwd -P)
 pushd $SETUPROOT
 
 # Run ansible playbooks
-ansible-playbook remote.yml lcd.yml vdr.yml kodi.yml kodi_config.yml
+ansible-playbook system.yml remote.yml lcd.yml vdr.yml kodi.yml kodi_config.yml
 
 # Run old setup routines
 sudo ./bin/setup.sh
