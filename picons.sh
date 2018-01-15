db12c622ca11265adfd8e1fe3fd583ec0359ce1f
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2018-01-15 21:50:55 +0100

    picons creation for VDR VNSI

---
+++ picons.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+echo "Setting up channel icons for Kodi VDR VNSI plugin ..."
+
+SETUPROOT=$(pushd $(dirname $0) > /dev/null; pwd -P)
+
+PICONS_SOURCE_DIR=$SETUPROOT/source
+PICONS_OUT_DIR=$SETUPROOT/vnsi
+
+# Get repo
+if [ ! -d "$PICONS_SOURCE_DIR" ]
+then
+   git clone https://github.com/picons/picons-source.git $PICONS_SOURCE_DIR
+fi
+
+pushd $PICONS_SOURCE_DIR
+
+git pull
+rm -rf build-input/* build-output/*
+echo '256x256;226x226;light;transparent' > build-input/backgrounds.conf
+cp -fv /var/lib/vdr/channels.conf build-input/
+./1-build-servicelist.sh srp
+./2-build-picons.sh srp
+rm -rf $PICONS_OUT_DIR
+mkdir -p $PICONS_OUT_DIR
+tar xvf build-output/binaries-srp/*.symlink.tar.xz --directory $PICONS_OUT_DIR
+mv $PICONS_OUT_DIR/srp*/* $PICONS_OUT_DIR/
+popd
+
+echo "Done."
